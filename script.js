f0bb02d7b220cb523b1a001588ac35fb1db18c40
Anastasiia Hmyrianska <a.hmyrianska@gmail.com>
2022-12-19 17:01:44 +0200

    Finished project

---
+++ script.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("form");
+const button = document.querySelector("button");
+const email = document.querySelector("input[type=email]");
+const errorMessage = document.querySelector(".error-text");
+const errorIcon = document.querySelector(".error-icon");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  email.blur();
+  let emailValue = email.value;
+
+  if (validateEmail(emailValue)) {
+    errorMessage.classList.add("hidden");
+    errorIcon.classList.add("hidden");
+    email.classList.remove("error-border");
+
+    console.log("test");
+    console.log(emailValue);
+    form.reset();
+  } else {
+    errorMessage.classList.remove("hidden");
+    errorIcon.classList.remove("hidden");
+    email.classList.add("error-border");
+  }
+});
+
+function validateEmail(email) {
+  var re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
